db02ce6592abbffb234fdac28fb8ade075352e05
ShenHongxuan <23302010060@m.fudan.edu.cn>
2025-04-23 20:50:35 +0800

    feat(coupon): give some TODO lists

--- backend/src/main/java/org/com/dianping/entity/Coupon.java
+++ backend/src/main/java/org/com/dianping/entity/Coupon.java
@@ -1,10 +1,10 @@
 package org.com.dianping.entity;
 
 public class Coupon {
+    //TODO
     public String getType() {
         return null;//TODO
     }
-
     public int getAmount() {
         return 0;//TODO
     }

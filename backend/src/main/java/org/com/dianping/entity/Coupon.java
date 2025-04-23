c8d04522cd5cadf594b3d85919c573bfdc0d143c
ShenHongxuan <23302010060@m.fudan.edu.cn>
2025-04-23 19:47:39 +0800

    feat(Coupon): remove wrong

--- backend/src/main/java/org/com/dianping/entity/Coupon.java
+++ backend/src/main/java/org/com/dianping/entity/Coupon.java
@@ -2,9 +2,11 @@ package org.com.dianping.entity;
 
 public class Coupon {
     public String getType() {
+        return null;
     }
 
     public int getAmount() {
+        return 0;
     }
     //TODO
 }

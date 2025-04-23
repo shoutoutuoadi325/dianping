ea010225fc59fb677db0264ad215792660d297d3
ShenHongxuan <23302010060@m.fudan.edu.cn>
2025-04-23 20:41:36 +0800

    fix(order): add a OrderRequest.java

---
+++ backend/src/main/java/org/com/dianping/DTO/OrderRequest.java
@@ -0,0 +1,26 @@
+package org.com.dianping.DTO;
+
+public class OrderRequest {
+    private Long packageId;
+    private Long couponId;
+
+    public OrderRequest() {
+
+    }
+    public OrderRequest(Long packageId, Long couponId) {
+        this.packageId = packageId;
+        this.couponId = couponId;
+    }
+    public Long getPackageId() {
+        return packageId;
+    }
+    public void setPackageId(Long packageId) {
+        this.packageId = packageId;
+    }
+    public Long getCouponId() {
+        return couponId;
+    }
+    public void setCouponId(Long couponId) {
+        this.couponId = couponId;
+    }
+}

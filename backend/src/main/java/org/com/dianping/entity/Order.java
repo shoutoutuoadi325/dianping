5c84b28ac43ee1c70003ae0ab397c50edf40d923
ShenHongxuan <23302010060@m.fudan.edu.cn>
2025-04-23 20:38:43 +0800

    fix(order): add a Order.java

--- backend/src/main/java/org/com/dianping/entity/Order.java
+++ backend/src/main/java/org/com/dianping/entity/Order.java
@@ -11,21 +11,64 @@ public class Order {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
-    @Column(name = "user_id")
+    @Column(nullable = false)
     private Long userId;
 
-    @Column(name = "package_id")
+    @Column(nullable = false)
     private Long packageId;
 
-    @Column(name = "coupon_code", unique = true)
-    private String couponCode;
+    @Column(nullable = false)
+    private LocalDateTime createTime;
 
-    private Double packagePrice;
-    private Double discount;
+    @Column(nullable = false)
+    private String businessName;
+
+    private Long couponId;
+
+    @Column(nullable = false)
+    private Double originalPrice;
+
+    @Column(nullable = false)
     private Double finalPrice;
 
-    @Column(name = "business_id")
-    private Long businessId;
+    @Column(nullable = false, unique = true)
+    private String voucherCode;
 
-    private LocalDateTime createTime = LocalDateTime.now();
+    @Column(nullable = false)
+    private String status; // 订单状态：未使用/已使用
+
+    public Order() {
+        super();
+    }
+
+    public void setUserId(Long userId) {
+        this.userId = userId;
+    }
+    public void setPackageId(Long packageId) {
+        this.packageId = packageId;
+    }
+    public void setCreateTime(LocalDateTime createTime) {
+        this.createTime = createTime;
+    }
+    public void setBusinessName(String businessName) {
+        this.businessName = businessName;
+    }
+    public void setCouponId(Long couponId) {
+        this.couponId = couponId;
+    }
+    public void setOriginalPrice(Double originalPrice) {
+        this.originalPrice = originalPrice;
+    }
+    public void setFinalPrice(Double finalPrice) {
+        this.finalPrice = finalPrice;
+    }
+    public void setVoucherCode(String voucherCode) {
+        this.voucherCode = voucherCode;
+    }
+    public void setStatus(String status) {
+        this.status = status;
+    }
+    public Long getId() {
+        return id;
+    }
 }

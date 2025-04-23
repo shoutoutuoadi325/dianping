1f716075f666fb444e075f16d3f619597b7cbde5
ShenHongxuan <23302010060@m.fudan.edu.cn>
2025-04-23 20:40:22 +0800

    fix(coupon): add a Service comment

--- backend/src/main/java/org/com/dianping/service/CouponService.java
+++ backend/src/main/java/org/com/dianping/service/CouponService.java
@@ -1,9 +1,10 @@
 package org.com.dianping.service;
 
 import org.com.dianping.entity.Coupon;
+import org.springframework.stereotype.Service;
 
 import java.util.List;
-
+@Service
 public class CouponService {
     //TODO
     public List<Coupon> getValidCouponsForUser(Long userId) {

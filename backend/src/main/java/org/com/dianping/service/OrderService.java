300947d24530eb9176dc8808ea821a9e076a7a7a
ShenHongxuan <23302010060@m.fudan.edu.cn>
2025-04-23 20:44:23 +0800

    fix(order): remove a comment

--- backend/src/main/java/org/com/dianping/service/OrderService.java
+++ backend/src/main/java/org/com/dianping/service/OrderService.java
@@ -11,7 +11,6 @@ import org.com.dianping.repository.PackageGroupRepository;
 import org.springframework.stereotype.Service;
 
 import java.time.LocalDateTime;
-import java.util.Optional;
 import java.util.UUID;
 
 @Service

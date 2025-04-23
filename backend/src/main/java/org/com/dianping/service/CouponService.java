85e07a90dd0e7e0d9ed147a50d6132ac0269faa8
ShenHongxuan <23302010060@m.fudan.edu.cn>
2025-04-23 20:35:20 +0800

    feat(Coupon): add a CouponService.java

---
+++ backend/src/main/java/org/com/dianping/service/CouponService.java
@@ -0,0 +1,12 @@
+package org.com.dianping.service;
+
+import org.com.dianping.entity.Coupon;
+
+import java.util.List;
+
+public class CouponService {
+    //TODO
+    public List<Coupon> getValidCouponsForUser(Long userId) {
+        return null;//TODO
+    }
+}

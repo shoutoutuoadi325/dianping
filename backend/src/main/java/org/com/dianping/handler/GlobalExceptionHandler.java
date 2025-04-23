ae1d46c6daf6ce8c7017908964a1a1fab3feb33e
ShenHongxuan <23302010060@m.fudan.edu.cn>
2025-04-23 21:01:12 +0800

    feat(handler): delete a null line

--- backend/src/main/java/org/com/dianping/handler/GlobalExceptionHandler.java
+++ backend/src/main/java/org/com/dianping/handler/GlobalExceptionHandler.java
@@ -6,7 +6,6 @@ import org.springframework.web.bind.annotation.RestControllerAdvice;
 
 @RestControllerAdvice
 public class GlobalExceptionHandler {
-
     @ExceptionHandler(RuntimeException.class)
     public ResponseEntity<?> handleRuntimeException(RuntimeException e) {
         return ResponseEntity.badRequest().body(new ErrorResponse(e.getMessage()));

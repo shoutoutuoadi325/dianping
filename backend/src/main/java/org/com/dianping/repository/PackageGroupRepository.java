2c0eb59e41055cf5947fb9a34fb51da660ee5df8
ShenHongxuan <23302010060@m.fudan.edu.cn>
2025-05-06 14:18:44 +0800

    add files

--- backend/src/main/java/org/com/dianping/repository/PackageGroupRepository.java
+++ backend/src/main/java/org/com/dianping/repository/PackageGroupRepository.java
@@ -1,16 +1,18 @@
 package org.com.dianping.repository;
 
-import org.com.dianping.DTO.OrderRequest;
 import org.com.dianping.entity.PackageGroup;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 
+import java.util.List;
 
 public interface PackageGroupRepository extends JpaRepository<PackageGroup, Long> {
+    // 根据商户ID查询套餐列表
+    List<PackageGroup> findByMerchantId(Long merchantId);
+
     @Modifying
     @Query("UPDATE PackageGroup p SET p.sales = p.sales + 1 WHERE p.id = :packageId")
     void incrementSales(@Param("packageId") long packageId);
-
 }
